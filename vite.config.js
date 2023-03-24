0b028353d7b6b16df869d472877d09e9564fd6fe
Smayan Nirantare <smayannirantare5@gmail.com>
2023-03-23 21:07:12 -0700

    updated base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "https://github.com/Smayan-n/pathfinding-visualizer/tree/gh-pages",
 	plugins: [react()],
 });
